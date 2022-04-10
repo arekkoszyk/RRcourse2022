aa46a156a97ca6388a9fb3c7215245b8fd140275
Arek Koszyk <arek.koszyk@gmail.com>
2022-04-10 02:32:30 +0200

    The loop for the assignment 3

--- Assignment3.R
+++ Assignment3.R
@@ -1,1 +1,7 @@
-dd
+setwd("/Users/arek/Desktop/RR/RRcourse2022/RR_Mar_30_31/")
+library(rmarkdown)
+
+for(s in 1:8) {
+  render("Exercise_2.Rmd", params = list(season = s),
+         output_file = paste0("report_season_", s, ".html"))
+}
