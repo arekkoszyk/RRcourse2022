3c65f4db5eddfef44a0ab69ad2b2da02751a792d
Arek Koszyk <arek.koszyk@gmail.com>
2022-05-05 15:51:03 +0200

    Adding the exercise from Mai 5th

---
+++ Exercise_0505.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+
+def convert(f, target='c'):
+    if target == 'c':
+        return (f - 32) / 1.8
+    elif target == 'k':
+        return ((f - 32) / 1.8) + 273.15
+    else:
+        raise Exception('wrong target')
+        
+class TestDivide(unittest.TestCase):
+    def test_divide_floats(self):
+        self.assertEqual(convert(50), 10)
+        self.assertEqual(convert(70), 21.1111111)
+        self.assertEqual(convert(90), 32.2222222)
+
+if __name__ == '__main__':
+    unittest.main()
+# Check whether 50, 70, and 90 degrees Fahrenheit are converted correctly to Celsius.
+# print(convert(50))
